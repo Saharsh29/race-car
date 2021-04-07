0a640efc19c7cff1ae24f018eb3ce6eaa49003c6
Saharsh29 <79233329+Saharsh29@users.noreply.github.com>
2021-04-07 19:43:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
-car1_width = 120;
-car1_height = 70;
-car1_image = "https://i.postimg.cc/9rqYz9HM/car1.png";
-car1_x = 10;
-car1_y = 10;
+car4_width = 120;
+car4_height = 70;
+car4_image = "car_4.jpg";
+car4_x = 10;
+car4_y = 10;
 
 car2_width = 120;
 car2_height = 70;
-car2_image = "https://i.postimg.cc/tnnW1Kff/car2.png";
+car2_image = "car_2.jpg";
 car2_x = 10;
 car2_y = 100;
 
-background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
+background_image = "car_5.jpg";
 
 function add() {
 	background_imgTag = new Image(); //defining a variable with a new image
 	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
 	background_imgTag.src = background_image;   // load image
 
-	car1_imgTag = new Image(); //defining a variable with a new image
-	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
-	car1_imgTag.src = car1_image;   // load image
+	car4_imgTag = new Image(); //defining a variable with a new image
+	car4_imgTag.onload = uploadcar4; // setting a function, onloading this variable
+	car4_imgTag.src = car4_image;   // load image
 
 	car2_imgTag = new Image(); //defining a variable with a new image
 	car2_imgTag.onload = uploadcar2; // setting a function, onloading this variable
@@ -33,8 +33,8 @@ function uploadBackground() {
 	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
-function uploadcar1() {
-	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+function uploadcar4() {
+	ctx.drawImage(car4_imgTag, car4_x, car4_y, car4_width, car4_height);
 }
 function uploadcar2() {
 	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
@@ -49,22 +49,22 @@ function my_keydown(e)
 	console.log(keyPressed);
 		if(keyPressed == '38')
 		{
-			car1_up();
+			car4_up();
 			console.log("up arrow key");
 		}
 		if(keyPressed == '40')
 		{
-			car1_down();
+			car4_down();
 			console.log("down arrow key");
 		}
 		if(keyPressed == '37')
 		{
-			car1_left();
+			car4_left();
 			console.log("left arrow key");
 		}
 		if(keyPressed == '39')
 		{
-			car1_right();
+			car4_right();
 			console.log("right arrow key");
 		}
 		if(keyPressed == '87')
@@ -89,3 +89,50 @@ function my_keydown(e)
 		}
 }
 
+car4_up()
+{
+	if(car4_y >= 0)
+	{
+		car4_y = car4_y - 10;
+		console.log("When arrow is pressed, x = " + car4_y + " | y =" +car4_y)
+		uploadBackground();
+		uploadcar4();
+		uploadcar2();
+	}
+}
+
+car4_down()
+{
+	if(car4_y <= 500)
+	{
+		car4_y = car4_y + 10;
+		console.log("When arrow is pressed, x = " + car4_y + " | y =" +car4_x)
+		uploadBackground();
+		uploadcar4();
+		uploadcar2();
+	}
+}
+
+car4_left()
+{
+	if(car4_x >= 0)
+	{
+		car4_x = car4_x - 10;
+		console.log("When arrow is pressed, x = " + car4_x + " | y =" +car4_y)
+		uploadBackground();
+		uploadcar4();
+		uploadcar2();
+	}
+}
+
+car4_right()
+{
+	if(car4_x >= 500)
+	{
+		car4_x = car4_x + 10;
+		console.log("When arrow is pressed, x = " + car4_x + " | y =" +car4_y)
+		uploadBackground();
+		uploadcar4();
+		uploadcar2();
+	}
+}
